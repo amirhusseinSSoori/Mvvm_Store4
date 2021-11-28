5863e12551992dd31c63c2ebc03dd199e140c83e
Soori <soori@hamtasharif.ir>
2021-11-28 07:59:36 -0800

    change token v3

--- app/src/main/java/com/example/myapplication/common/Constance.kt
+++ app/src/main/java/com/example/myapplication/common/Constance.kt
@@ -2,7 +2,7 @@ package com.example.myapplication.common
 
 object Constance {
     const val BaseUrl = "https://api.github.com/graphql"
-    const val TOKEN = "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"
+    const val TOKEN = "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"
     const val DbName = "DATABASE_NAME"
     const val Problem = "خظا در ارتباط با سرور"
     const val KeyStream = "github"
