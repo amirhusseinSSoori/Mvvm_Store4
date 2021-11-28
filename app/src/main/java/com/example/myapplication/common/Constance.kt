c80ed740dba8a7ffd8d373c21c310efcb3007ac3
Soori <soori@hamtasharif.ir>
2021-11-28 07:48:20 -0800

    add dependency

--- app/src/main/java/com/example/myapplication/common/Constance.kt
+++ app/src/main/java/com/example/myapplication/common/Constance.kt
@@ -2,7 +2,7 @@ package com.example.myapplication.common
 
 object Constance {
     const val BaseUrl = "https://api.github.com/graphql"
-    const val TOKEN = "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"
+    const val TOKEN = "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"
     const val DbName = "DATABASE_NAME"
     const val Problem = "خظا در ارتباط با سرور"
     const val KeyStream = "github"
